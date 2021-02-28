5755355ad91a9b07189f57d6761adeae252d894a
jaimegsandoval <jaime.devs@gmail.com>
2021-02-27 19:31:54 -0800

    Working on navbar styles

--- js/scroll.js
+++ js/scroll.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 600, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
@@ -22,3 +22,72 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+const navToggler = $('.navbar-toggler');
+
+$("#pb-navbar ul li a[href^='#']").on('click', function (e) {
+    if (navToggler.is(':visible')) {
+        navToggler.click();
+    }
+});
+
+
+$(document).ready(function() {
+  // var stickyNavTop = $('.navbar').offset().top;
+
+  const stickyNav = function(){
+    // var scrollTop = $(window).scrollTop();
+// scrollTop > stickyNavTop
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300 ) {
+      $('.navbar').addClass('sticky');
+      $('.navbar').addClass('white');
+    } else {
+      $('.navbar').removeClass('sticky');
+    }
+  };
+
+  stickyNav();
+
+  $(window).scroll(function() {
+    stickyNav();
+  });
+});
+
+
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+//     document.querySelector(".navbar").style.position = 'sticky';
+//     document.querySelector(".navbar").style.top = "100";
+//   } else {
+//   }
+// }
+
+
+    // var OnePageNav = function () {
+    //     var navToggler = $('.navbar-toggler');
+    //     $(".smoothscroll[href^='#'], #pb-navbar ul li a[href^='#']").on('click', function (e) {
+    //         e.preventDefault();
+    //         var hash = this.hash;
+
+    //         $('html, body').animate({
+
+    //             scrollTop: $(hash).offset().top
+    //         }, 700, 'easeInOutExpo', function () {
+    //             window.location.hash = hash;
+    //         });
+    //     });
+    //     $("#pb-navbar ul li a[href^='#']").on('click', function (e) {
+    //         if (navToggler.is(':visible')) {
+    //             navToggler.click();
+    //         }
+    //     });
+
+    //     $('body').on('activate.bs.scrollspy', function () {
+    //         console.log('nice');
+    //     })
+    // };
