4882e17f37bff84f1adfca6e124ad2a58634532d
jaimegsandoval <jaime.devs@gmail.com>
2022-03-08 20:02:20 -0800

    Add intersection observer for header for fixed nav

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// mobile navbar toggle functionality
 const navbarToggleButton = document.querySelector('.navbar-toggle');
 const navbarDropdown = document.querySelector('.navbar-dropdown');
 
@@ -18,3 +19,23 @@ window.onclick = (event) => {
     dropDown.classList.remove('showDropdown');
   }
 };
+
+// fixed nav on scroll
+const header = document.querySelector('.header');
+const navbar = document.querySelector('.navbar');
+
+const fixedNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  console.log(window.scrollY);
+  if (!entry.isIntersecting) navbar.classList.add('fixedNav');
+  else navbar.classList.remove('fixedNav');
+};
+
+const headerObserver = new IntersectionObserver(fixedNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `250px`, // function fires when header is 250px outside the viewport, or 250px within the viewport. The value of true means that yes, the header/observed target is outside the viewport by 250px. A value of false means that no, the header/observed element is is not 250px outside the viewport i.e., it's still within the viewport or outside the viewport by less than 250px
+});
+
+headerObserver.observe(header);
